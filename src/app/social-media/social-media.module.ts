8fe8a7fa1991a87f936bc10f7fa5460430e00698
raed ABBAS <raed.abbas@hotmail.fr>
2022-11-11 12:07:51 +0100

    debut, services, social media module

---
+++ src/app/social-media/social-media.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SocialMediaRoutingModule } from './social-media-routing.module';
+import { PostService } from './services/post.service';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SocialMediaRoutingModule
+  ],
+  providers:[
+    PostService
+  ]
+})
+export class SocialMediaModule { }
