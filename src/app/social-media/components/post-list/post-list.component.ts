6672689d20b75df7d7c44cac2f484ceb70e65b15
raed ABBAS <raed.abbas@hotmail.fr>
2022-11-13 04:13:46 +0100

    gerer les commentaires

--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { Post } from '../../models/post.model';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post-list',
@@ -9,14 +10,17 @@ import { Post } from '../../models/post.model';
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
+
   posts$!:Observable<Post[]>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private postService:PostService) { }
 
   ngOnInit(): void {
     this.posts$=this.route.data.pipe(
       map(data=>data['posts'])
     );
   }
-
+  onPostCommented(postCommented: { comment: string; postId: number; }) {
+    this.postService.addNewComment(postCommented);
+    }
 }
