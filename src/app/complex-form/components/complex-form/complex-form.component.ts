fd481ddd5e3061339862c447b72dead88c10017f
raed ABBAS <raed.abbas@hotmail.fr>
2022-11-14 19:00:09 +0100

    corrigé de fonction de radion email, phone

--- src/app/complex-form/components/complex-form/complex-form.component.ts
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { map, startWith, tap } from 'rxjs';
+import { map, Observable, startWith, tap } from 'rxjs';
 
 @Component({
   selector: 'app-complex-form',
@@ -10,108 +10,75 @@ import { map, startWith, tap } from 'rxjs';
 export class ComplexFormComponent implements OnInit {
 
   mainForm!: FormGroup;
-  personalInfoForm!: FormGroup;
-  contactPreferenceCtrl!: FormControl;
-  emailCtrl!: FormControl;
-  confirmEmailCtrl!: FormControl;
-  emailForm!: FormGroup;
- phoneCtrl!: FormControl;
-passwordCtrl!:FormControl;
-confirmPasswordCtrl!:FormControl;
-loginInfoForm!:FormGroup;
+  personalInfoForm!:FormGroup;
+  contactPreferenceCtrl!:FormControl;
+  phoneCtrl!:FormControl;
+  emailCtrl!:FormControl;
+  confirmEmailCtrl!:FormControl;
+  emailForm!:FormGroup;
+  passwordCtrl!:FormControl;
+  confirmPasswordCtrl!:FormControl;
+  loginInfoForm!:FormGroup;
+  showEmailCtrl$!:Observable<boolean>;
+  showPhoneCtrl$!:Observable<boolean>;
 
-  showEmailCtrl$: any;
-  showPhoneCtrl$: any;
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.initFormControls();
     this.initMainForm();
     this.initFormObservables();
-
-
   }
   private initFormObservables() {
-    this.showEmailCtrl$ = this.contactPreferenceCtrl.valueChanges.pipe(
-        startWith(this.contactPreferenceCtrl.value),
-        map(preference => preference === 'email'),
-        tap(showEmailCtrl => this.setEmailValidators(showEmailCtrl))
+    this.showEmailCtrl$=this.contactPreferenceCtrl.valueChanges.pipe(
+      startWith(this.contactPreferenceCtrl.value),
+      map(preferenc=>preferenc==='email')
     );
-    this.showPhoneCtrl$ = this.contactPreferenceCtrl.valueChanges.pipe(
-        startWith(this.contactPreferenceCtrl.value),
-        map(preference => preference === 'phone'),
-        tap(showPhoneCtrl => this.setPhoneValidators(showPhoneCtrl))
-    );
-}
-
-private setEmailValidators(showEmailCtrl: boolean) {
-    if (showEmailCtrl) {
-        this.emailCtrl.addValidators([
-            Validators.required,
-            Validators.email
-        ]);
-        this.confirmEmailCtrl.addValidators([
-            Validators.required,
-            Validators.email
-        ]);
-    } else {
-        this.emailCtrl.clearValidators();
-        this.confirmEmailCtrl.clearValidators();
-    }
-    this.emailCtrl.updateValueAndValidity();
-    this.confirmEmailCtrl.updateValueAndValidity();
-}
-
-private setPhoneValidators(showPhoneCtrl: boolean) {
-    if (showPhoneCtrl) {
-        this.phoneCtrl.addValidators([
-            Validators.required,
-            Validators.minLength(10),
-            Validators.maxLength(10)
-        ]);
-    } else {
-        this.phoneCtrl.clearValidators();
-    }
-    this.phoneCtrl.updateValueAndValidity();
-}
+    this.showPhoneCtrl$=this.contactPreferenceCtrl.valueChanges.pipe(
+      startWith(this.contactPreferenceCtrl.value),
+      map(preference=>preference==='phone'),
+      tap(showPhoneCtrl=>{
+        if(showPhoneCtrl){
+this.phoneCtrl.addValidators([Validators.required,Validators.minLength(10),Validators.maxLength(10)])
+        }else{
+this.phoneCtrl.clearValidators();
+        }
+        this.phoneCtrl.updateValueAndValidity();
+      })
+    )
+  }
+  private initFormControls() {
+this.personalInfoForm=this.formBuilder.group({
+  firstName:['',Validators.required],
+  lastName:['',Validators.required]
+});
+this.contactPreferenceCtrl=this.formBuilder.control('email');
+this.phoneCtrl=this.formBuilder.control('');
+this.emailCtrl=this.formBuilder.control('');
+this.confirmEmailCtrl=this.formBuilder.control('');
+this.emailForm=this.formBuilder.group({
+  email:this.emailCtrl,
+  confirm:this.confirmEmailCtrl
+});
+this.passwordCtrl=this.formBuilder.control('');
+this.confirmPasswordCtrl=this.formBuilder.control('');
+this.loginInfoForm=this.formBuilder.group({
+ username:['',Validators.required],
+password:this.passwordCtrl,
+confirmPassword:this.confirmPasswordCtrl
+})
+  }
 
   private initMainForm(): void {
     this.mainForm = this.formBuilder.group({
-      personalInfo: this.personalInfoForm,
-      contactPreference: this.contactPreferenceCtrl,
-      email: this.emailForm,
-      phone: this.phoneCtrl,
-      loginInfo: this.loginInfoForm
+      personalInfo:this.personalInfoForm,
+      contactPreference:this.contactPreferenceCtrl,
+      email:this.emailForm,
+      phone:this.phoneCtrl,
+      login:this.loginInfoForm
     });
   }
-  private initFormControls(): void {
-    this.personalInfoForm = this.formBuilder.group({
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required]
-    });
-    this.contactPreferenceCtrl = this.formBuilder.control('email');
-    this.emailCtrl=this.formBuilder.control('');
-    this.confirmEmailCtrl=this.formBuilder.control('');
-    this.emailForm=this.formBuilder.group({
-      email:this.emailCtrl,
-      confirm:this.confirmEmailCtrl
-    });
-    this.phoneCtrl = this.formBuilder.control('');
-    this.passwordCtrl=this.formBuilder.control('',Validators.required);
-    this.confirmPasswordCtrl=this.formBuilder.control('',Validators.required);
-    this.loginInfoForm=this.formBuilder.group({
-      username:['',Validators.required],
-      password:this.passwordCtrl,
-      confirmPassword:this.confirmPasswordCtrl
-    })
-}
-
-
   onSubmitForm() {
 console.log(this.mainForm.value)
   }
 }
-
-
-
-
